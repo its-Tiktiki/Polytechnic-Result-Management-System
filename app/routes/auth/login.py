0a42af27855c7c47d60ee0edfb72ac1a2ee7a306
rakibhossendev <rh01828315879@gmail.com>
2026-05-27 13:53:45 +0600

    add css styles

--- app/routes/auth/login.py
+++ app/routes/auth/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,redirect,render_template,request,url_for,flash
+from flask import Blueprint, redirect, render_template, request, url_for, flash
 from app.utils.form import LoginForm
 from app.extensions import db
 from app.models.admin import Admin
@@ -7,36 +7,24 @@ from app.models.admin import Admin
 DEFUALT_USERNAME = "admin"
 DEFUALT_PASSWORD = "admin123"
 
-#admin = Admin(
-   # DEFUALT_USERNAME=DEFUALT_USERNAME,
-  #  DEFUALT_PASSWORD=DEFUALT_PASSWORD
-#)
-
-#db.session.add(admin)
-#db.session.commit()
-
 # make blueprint and route
-login_bp = Blueprint("login",__name__,url_prefix="/login")
+login_bp = Blueprint("login", __name__, url_prefix="/login")
 
-@login_bp.route("/login",methods=["GET","POST"])
+@login_bp.route("/", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
 
     if login_form.validate_on_submit():
-          username = login_form.username.data
-          password = login_form.password.data
+        username = login_form.username.data
+        password = login_form.password.data
 
-          print("username:",username)
+        if username == DEFUALT_USERNAME and password == DEFUALT_PASSWORD:
+            return "Login success"
+        else:
+            return "invalid username or password"
 
-          if username == DEFUALT_USERNAME and password == DEFUALT_PASSWORD:
-                return "Login success"
-          else:
-                return "invalid username or password"
-
-          
-
-    return render_template("/auth/login.html",login_form=login_form)
+    return render_template("auth/login.html", login_form=login_form)
 
 @login_bp.route("/admin")
 def admin_dashboard():
-     return "login success"
+    return "login success"

4354029ff78b28e91dd2d4d7b84b9ac49dbaf461
rakibhossendev <rh01828315879@gmail.com>
2026-06-26 09:45:11 +0600

    add features of total student views system

--- app/routes/teacher/teacher_dashboard.py
+++ app/routes/teacher/teacher_dashboard.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,redirect,render_template,session,url_for
+from app.models.teacher import AddStudentInfo
 
 teacher_dashboard_bp = Blueprint(
     "teacher_dashboard",
@@ -10,5 +11,8 @@ teacher_dashboard_bp = Blueprint(
 def teacher_dashboard():
     if not session.get("teacher"):
         return redirect(url_for("login.login"))
+
+    teacher_id = session.get("teacher_id") 
+    total_student = AddStudentInfo.query.filter_by(teacher_id=teacher_id).count()
     
-    return render_template("teacher/teacher_dashboard.html")
+    return render_template("teacher/teacher_dashboard.html",total_student=total_student)
